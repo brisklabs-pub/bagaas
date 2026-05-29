fdd7837dacc954310bb755db3d43484a992d0b80
rogermolas <r.molas06@gmail.com>
2026-05-30 01:23:13 +0800

    add new structure

--- js/modal.js
+++ js/modal.js
@@ -1,262 +1,131 @@
+/* =========================================
+ ARTICLES
+========================================= */
 const newsGrid = document.getElementById("newsGrid");
-
 async function loadArticles() {
-  const response =
-    await fetch("https://raw.githubusercontent.com/brisklabs-pub/bagaas/refs/heads/main/data/articles.json");
-  const articles =
-    await response.json();
+  const response = await fetch("https://raw.githubusercontent.com/brisklabs-pub/bagaas/refs/heads/main/data/articles.json");
+  const articles = await response.json();
 
   articles.forEach((article) => {
-
-    const card =
-      document.createElement("div");
-
-    card.className =
-      "news-card";
-
+    const card = document.createElement("div");
+    card.className = "news-card";
     card.innerHTML = `
-      <img
-        src="${article.thumbnail}"
-        alt="${article.title}"
-      />
-
+      <img src="${article.thumbnail}" alt="${article.title}"/>
       <div class="news-body">
+        <span class="news-date">${article.datetime}</span>
+        <h3>${article.title}</h3>
+        <p>${article.content}</p>
+        <button class="read-more"> Read More → </button>
+      </div>`;
 
-        <span class="news-date">
-          ${article.datetime}
-        </span>
-
-        <h3>
-          ${article.title}
-        </h3>
-
-        <p>
-          ${article.content}
-        </p>
-
-        <button class="read-more">
-          Read More →
-        </button>
-
-      </div>
-    `;
-
-    card.onclick = () =>
-      openContentModal(article);
-
+    card.onclick = () => openContentModal(article);
     newsGrid.appendChild(card);
-
   });
 }
-
 function copyLink() {
   navigator.clipboard.writeText(window.location.href);
   alert("Link copied!");
 }
-
-// Acticles
+// LOAD ARTICLES
 loadArticles();
-
 
 /* =========================================
  ANNOUNCEMENTS
 ========================================= */
-
 const announcementGrid = document.getElementById("announcementGrid");
 async function loadAnnouncements() {
   try {
-
-    const response =
-      await fetch(
-        "https://raw.githubusercontent.com/brisklabs-pub/bagaas/refs/heads/main/data/announce.json"
-      );
-
-    const announcements =
-      await response.json();
-
+    const response = await fetch("https://raw.githubusercontent.com/brisklabs-pub/bagaas/refs/heads/main/data/announce.json");
+    const announcements = await response.json();
     announcementGrid.innerHTML = "";
-
     announcements.forEach(
       (announcement, index) => {
-
-        const card =
-          document.createElement("div");
-
+        const card = document.createElement("div");
         /* FEATURED */
-        if (
-          announcement.type === "important"
-          && index === 0
-        ) {
-
-          card.className =
-            "announcement-card featured";
-
+        if (announcement.type === "important" && index === 0) {
+          card.className = "announcement-card featured";
           card.innerHTML = `
-
-                    <div class="announcement-badge">
-                        IMPORTANT
-                    </div>
-
-                    <div class="announcement-content">
-
-                        <span class="announcement-date">
-                            ${announcement.date}
-                        </span>
-
-                        <h3>
-                            ${announcement.title}
-                        </h3>
-
-                        <p>
-                            ${announcement.content}
-                        </p>
-
-                        <a href="#">
-                            Read More →
-                        </a>
-
-                    </div>
-                `;
-
+          <div class="announcement-badge">IMPORTANT</div>
+          <div class="announcement-content">
+            <span class="announcement-date">
+              ${announcement.date}
+            </span>
+              
+            <h3>${announcement.title}</h3>
+            <p>${announcement.content}</p>
+            <a href="#">Read More →</a>
+          </div>`;
         } else {
-
-          card.className =
-            "announcement-card";
-
+          card.className = "announcement-card";
           card.innerHTML = `
-
-                    <div class="announcement-icon">
-
-                        ${announcement.type === "important"
-              ? "📢"
-              : "📌"
-            }
-
-                    </div>
-
-                    <div class="announcement-content">
-
-                        <span class="announcement-date">
-                            ${announcement.date}
-                        </span>
-
-                        <h3>
-                            ${announcement.title}
-                        </h3>
-
-                        <p>
-                            ${announcement.content}
-                        </p>
-
-                    </div>
-                `;
+          <div class="announcement-icon">
+            ${announcement.type === "important" ? "📢" : "📌"}
+          </div>
+          <div class="announcement-content">
+            <span class="announcement-date">
+              ${announcement.date}
+            </span>
+            <h3>${announcement.title}</h3>
+            <p>${announcement.content}</p>
+          </div>`;
         }
-
         /* OPEN MODAL */
         card.onclick = () => {
-
           openContentModal({
-
-            category:
-              "Announcement",
-
-            title:
-              announcement.title,
-
-            date:
-              announcement.date,
-
-            content:
-              announcement.content,
-
-            image:
-              announcement.image || ""
-
+            category:"Announcement",
+            title: announcement.title,
+            date: announcement.date,
+            content: announcement.content,
+            image: announcement.image || ""
           });
         };
-
         announcementGrid.appendChild(card);
-
       });
 
   } catch (error) {
-
-    console.error(
-      "Failed loading announcements:",
-      error
-    );
-
+    console.error("Failed loading announcements:",error);
     announcementGrid.innerHTML = `
-            <p class="empty-content">
-                Failed to load announcements.
-            </p>
-        `;
+      <p class="empty-content">
+        Failed to load announcements.
+      </p>`;
   }
 }
-
-/* LOAD */
+/* LOAD ANNOUNCEMENTS*/
 loadAnnouncements();
 
-//=====================================================================
+/* =========================================
+ FACILITIES
+========================================= */
 let facilities = [];
 let currentFacility = 0;
 
 async function loadFacilities() {
   const response = await fetch("https://raw.githubusercontent.com/brisklabs-pub/bagaas/refs/heads/main/data/facilities.json");
   facilities = await response.json();
-
-  const facilityGrid =
-    document.getElementById(
-      "facilityGrid"
-    );
+  const facilityGrid = document.getElementById("facilityGrid");
 
   facilities.forEach(
     (facility, index) => {
-
-      const item =
-        document.createElement("div");
-
-      item.className =
-        "facility-item";
-
+      const item = document.createElement("div");
+      item.className = "facility-item";
       item.innerHTML = `
-        <img
-          src="${facility.image}"
-          alt="${facility.title}"
-        />
-
+        <img src="${facility.image}" alt="${facility.title}"/>
         <div class="facility-label">
-
-          <h3>
-            ${facility.title}
-          </h3>
-
-        </div>
-      `;
-
-      item.onclick = () =>
-        openFacilityModal(index);
-
+          <h3>${facility.title}</h3>
+        </div>`;
+      item.onclick = () => openFacilityModal(index);
       facilityGrid.appendChild(item);
     }
   );
 }
-
 function openFacilityModal(index) {
   currentFacility = index;
   updateFacilityModal();
-  document.getElementById(
-    "facilityModal"
-  ).classList.add("show");
+  document.getElementById("facilityModal").classList.add("show");
 }
-
 function closeFacilityModal() {
-
-  document.getElementById(
-    "facilityModal"
-  ).classList.remove("show");
+  document.getElementById("facilityModal").classList.remove("show");
 }
-
 function nextFacility() {
   currentFacility++;
   if (currentFacility >= facilities.length) {
@@ -264,7 +133,6 @@ function nextFacility() {
   }
   updateFacilityModal();
 }
-
 function prevFacility() {
   currentFacility--;
   if (currentFacility < 0) {
@@ -272,18 +140,10 @@ function prevFacility() {
   }
   updateFacilityModal();
 }
-
 function updateFacilityModal() {
   const facility = facilities[currentFacility];
-  document.getElementById(
-    "facilityModalImage"
-  ).src =
-    facility.image;
-
-  document.getElementById(
-    "facilityModalTitle"
-  ).innerText =
-    facility.title;
+  document.getElementById("facilityModalImage").src = facility.image;
+  document.getElementById("facilityModalTitle").innerText = facility.title;
 }
 
 // Facilities
@@ -297,21 +157,17 @@ function openContentModal(data) {
   const modal = document.getElementById("contentModal");
   const container = document.querySelector(".content-modal-container");
   const image = document.getElementById("contentModalImage");
-
   /* CATEGORY */
   document.getElementById("contentModalCategory").innerText = data.category || "";
   /* DATE */
-  document.getElementById("contentModalDate").innerText = data.date || "";
+  document.getElementById("contentModalDate").innerText = data.created_at || "";
   /* TITLE */
   document.getElementById("contentModalTitle").innerText = data.title || "";
   /* CONTENT */
   document.getElementById("contentModalContent").innerText = data.content || "";
 
   /* IMAGE */
-  const imageSource =
-    data.thumbnail ||
-    data.image ||
-    "";
+  const imageSource = data.thumbnail || data.image || "";
 
   if (imageSource) {
     image.src = imageSource;
@@ -320,27 +176,22 @@ function openContentModal(data) {
     image.src = "";
     container.classList.add("no-image");
   }
-
   /* OPEN */
   modal.classList.add("show");
   document.body.style.overflow = "hidden";
 }
 
 /* =========================================
-   CLOSE
+  CLOSE
 ========================================= */
-
 function closeContentModal() {
-  document
-    .getElementById("contentModal")
-    .classList.remove("show");
+  document.getElementById("contentModal").classList.remove("show");
   document.body.style.overflow = "auto";
 }
 
 /* =========================================
-   COPY LINK
+  COPY LINK
 ========================================= */
-
 function copyLink() {
   navigator.clipboard.writeText(window.location.href);
   alert("Link copied!");
